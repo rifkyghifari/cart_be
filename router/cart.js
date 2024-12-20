68b11ec51b0fa6f2ba6449f50e0a5c36ee43b4c0
Boyzest-Saragih <110437953+Boyzest-Saragih@users.noreply.github.com>
2024-12-20 17:48:34 +0700

    Update cart.js

--- router/cart.js
+++ router/cart.js
@@ -27,7 +27,7 @@ router.get("/", (req, res) => {
     if (err) {
       return res.json({ status: 200, error: true, result });
     } else {
-      return res.json({ status: 200, error: false, data: result });
+      return res.json({ status: 200, error: false, result });
     }
   });
 });
